c7a9939daa0336f6ca2ae55a890325213ac95263
P.J. van Geldrop <P.J.vanGeldrop@tilburguniversity.edu>
2026-07-10 12:22:56 +0200

    Update powershell script

--- pw_install.ps1
+++ pw_install.ps1
@@ -1,8 +1,17 @@
-# Playwright install needed to be able to download PDFs from Marimo
-$MinicondaPath = "C:\Miniconda3"
-$EnvName = "sane-env"
+# 1. Define paths
+$MinicondaPath = "C:\Miniconda3" # Update to your Conda path
+$EnvName = "sane-env"         # Update to your Conda environment name
+$SharedBrowserPath = "C:\PlaywrightBrowsers"
+
+New-Item -ItemType Directory -Force -Path $SharedBrowserPath | Out-Null
+
+[Environment]::SetEnvironmentVariable("PLAYWRIGHT_BROWSERS_PATH", $SharedBrowserPath, "Machine")
+
+$env:PLAYWRIGHT_BROWSERS_PATH = $SharedBrowserPath
 
 Import-Module "$MinicondaPath\shell\condabin\Conda.psm1"
-
 conda activate $EnvName
+
 playwright install chromium
+
+Write-Host "Playwright installation complete. The UI button will now work offline."
