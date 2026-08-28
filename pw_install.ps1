3329b622f77bdadc1eec13c5645edd28742a2d93
Paul van Geldrop <125973636+PvanGeldrop@users.noreply.github.com>
2026-08-28 16:22:02 +0200

    Update pw_install.ps1
    Add env var for PLAYWRIGHT_BROWSERS_PATH to system env variables.

--- pw_install.ps1
+++ pw_install.ps1
@@ -4,6 +4,8 @@ $SharedBrowserPath = "C:\PlaywrightBrowsers"
 $CondaExe = "$MinicondaPath\Scripts\conda.exe"
 
 New-Item -ItemType Directory -Force -Path $SharedBrowserPath | Out-Null
+[Environment]::SetEnvironmentVariable("PLAYWRIGHT_BROWSERS_PATH", $SharedBrowserPath, "Machine")
+
 $env:PLAYWRIGHT_BROWSERS_PATH = $SharedBrowserPath
 
 & $CondaExe run -n $EnvName python -m playwright install chromium 2>&1 | Write-Output
