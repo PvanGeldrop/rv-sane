d4e2fb05a2d322c25d2856f8615b14bfe384892c
Paul van Geldrop <125973636+PvanGeldrop@users.noreply.github.com>
2026-08-28 11:25:43 +0200

    Update gpu_pip_installs.ps1

--- gpu_pip_installs.ps1
+++ gpu_pip_installs.ps1
@@ -4,4 +4,4 @@ $EnvName = "sane-env"
 Import-Module "$MinicondaPath\shell\condabin\Conda.psm1"
 conda activate $EnvName
 
-python -m pip install  | Write-Host
+python -m pip install --index-url https://download.pytorch.org/whl/cu129 torch  | Write-Host
