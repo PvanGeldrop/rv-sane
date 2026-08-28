df3e587af86c6a46abfd1bf3bbc4a46c8ac61a4c
Paul van Geldrop <125973636+PvanGeldrop@users.noreply.github.com>
2026-08-28 15:15:06 +0200

    Update cpu_pip_installs.ps1
    Add playwright here instead of conda.

--- cpu_pip_installs.ps1
+++ cpu_pip_installs.ps1
@@ -2,4 +2,4 @@ $MinicondaPath = "C:\Miniconda3"
 $EnvName = "sane-env"
 $CondaExe = "$MinicondaPath\Scripts\conda.exe"
 
-& $CondaExe run -n $EnvName python -m pip install "torch==2.10.0" 2>&1 | Write-Host
+& $CondaExe run -n $EnvName python -m pip install "torch==2.10.0" playwright 2>&1 | Write-Host
