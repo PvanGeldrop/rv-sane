3d9393738ca20c89597146dd99645a53720b68dc
Paul van Geldrop <125973636+PvanGeldrop@users.noreply.github.com>
2026-08-28 14:19:11 +0200

    Delete pw_install_conda_run.ps1

--- pw_install_conda_run.ps1
+++
@@ -1,9 +0,0 @@
-$MinicondaPath = "C:\Miniconda3"
-$EnvName = "sane-env"
-$SharedBrowserPath = "C:\PlaywrightBrowsers"
-$CondaExe = "$MinicondaPath\Scripts\conda.exe"
-
-New-Item -ItemType Directory -Force -Path $SharedBrowserPath | Out-Null
-$env:PLAYWRIGHT_BROWSERS_PATH = $SharedBrowserPath
-
-& $CondaExe run -n $EnvName python -m playwright install chromium 2>&1 | Write-Output
