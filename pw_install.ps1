c24c792a5e9e18b2629d09cd020eba79e8b87ffc
Paul van Geldrop <125973636+PvanGeldrop@users.noreply.github.com>
2026-08-28 13:24:00 +0200

    Update pw_install.ps1

--- pw_install.ps1
+++ pw_install.ps1
@@ -1,14 +1,9 @@
 $MinicondaPath = "C:\Miniconda3"
 $EnvName = "sane-env"
 $SharedBrowserPath = "C:\PlaywrightBrowsers"
+$CondaExe = "$MinicondaPath\Scripts\conda.exe"
 
 New-Item -ItemType Directory -Force -Path $SharedBrowserPath | Out-Null
-
-[Environment]::SetEnvironmentVariable("PLAYWRIGHT_BROWSERS_PATH", $SharedBrowserPath, "Machine")
-
 $env:PLAYWRIGHT_BROWSERS_PATH = $SharedBrowserPath
 
-Import-Module "$MinicondaPath\shell\condabin\Conda.psm1" 2>&1  Write-Output
-Enter-CondaEnvironment $EnvName 2>&1 | Write-Output
-
-python -m playwright install chromium 2>&1 | Write-Output
+& $CondaExe run -n $EnvName python -m playwright install chromium 2>&1 | Write-Output
