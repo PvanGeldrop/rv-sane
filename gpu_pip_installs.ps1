68dee86c523c32e6d724e93e37812be60494980a
Paul van Geldrop <125973636+PvanGeldrop@users.noreply.github.com>
2026-08-28 15:15:26 +0200

    Update gpu_pip_installs.ps1
    Add playwright as pip install.

--- gpu_pip_installs.ps1
+++ gpu_pip_installs.ps1
@@ -2,4 +2,4 @@ $MinicondaPath = "C:\Miniconda3"
 $EnvName = "sane-env"
 $CondaExe = "$MinicondaPath\Scripts\conda.exe"
 
-& $CondaExe run -n $EnvName python -m  pip install --index-url https://download.pytorch.org/whl/cu129 torch  | Write-Host
+& $CondaExe run -n $EnvName python -m  pip install --index-url https://download.pytorch.org/whl/cu129 torch playwright  | Write-Host
